8712edf24a2a2f3faa4410a7202d95699153eee1
dbaylyuvic <dbayly@uvic.ca>
2017-11-09 23:45:04 -0800

    Fixed every last lint error

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
-var PORT = process.env.PORT || 8080; // default port 8080
+var PORT = process.env.PORT || 8080;
+// default port 8080
 app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -12,76 +13,97 @@ var cookieSession = require('cookie-session');
 app.use(cookieSession({
   name: 'session',
   keys: ['key1', 'key2']
-}))
+}));
 
 
 var urlDatabase = {
-  'b2xVn2':{"longURL": "http://www.lighthouselabs.ca",'user_id':"jack",'shortURL':'b2xVn2'},
-  '9sm5xk':{"longURL": "http://www.google.com",'user_id':"jack",'shortURL':'9sm5xk'}
+  'b2xVn2': {"longURL": "http://www.lighthouselabs.ca", 'userId': "jack", 'shortURL': 'b2xVn2'},
+  '9sm5xk': {"longURL": "http://www.google.com", 'userId': "jack", 'shortURL': '9sm5xk'}
 };
 
 
-const users={
+const users = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
- "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
+};
+function generateRandomString() {
+  let result = "";
+  for(var i = 0 ; i < 6 ; i++){
+    result += String.fromCharCode( 42 * Math.random() + 48);
+  }
+  console.log(result);
+  return result;
 }
-//GET METHODS
+function generateRandomUsername() {
+  let result = "";
+  for(var i = 0 ; i < 10; i++){
+    result += String.fromCharCode( 42 * Math.random() + 48);
+  }
+  console.log(result);
+  return 'usr' + result;
+}
+function listLinks(userId){
+  let result = [];
+  for(var x in urlDatabase){
+    if(urlDatabase[x]['userId'] === userId){
+      result.push(urlDatabase[x]);
+    }
+  }
+  return result;
+}
 app.get("/", (req, res) => {
-  if(req.session.user_id===undefined){
-    res.render("partials/_header.ejs",{user_id:undefined});
+  if(req.session.userId === undefined){
+    res.render("partials/_header.ejs", {userId: undefined});
   }else{
-    res.render("partials/_header.ejs",{user_id:req.session.user_id});
+    res.render("partials/_header.ejs", {userId: req.session.userId});
   }
 });
-
- 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 app.post("/register", (req, res) => {
   console.log(req.body);
-  let nme = req.body['user_id'];
+  let nme = req.body['userId'];
   let pwd = req.body['password'];
   let id = generateRandomUsername();
-  req.session.user_id=nme;
-  if(nme!=="" && pwd!==""){
-    users[id] = {id:id,email:nme,password:bcrypt.hashSync(pwd,10)}
+  req.session.userId = nme;
+  if(nme !== "" && pwd !== ""){
+    users[id] = {id: id, email: nme, password: bcrypt.hashSync(pwd, 10)};
     res.redirect('http://localhost:8080/urls');
   }else{
     res.send("invalid inputs");
   }
 });
 app.post("/urls", (req, res) => {
-   if(req.session.user_id===undefined){
-    res.render("partials/_header.ejs",{user_id:undefined});
+  if(req.session.userId === undefined){
+    res.render("partials/_header.ejs", {userId: undefined});
   }
-  //console.log(req.body);  // debug statement to see POST parameters
   let shortURL = generateRandomString();
-  urlDatabase[shortURL]={'longURL':req.body['longURL'],'user_id':req.session.user_id,'shortURL':shortURL};
+  urlDatabase[shortURL] = {'longURL': req.body['longURL'], 'userId': req.session.userId, 'shortURL': shortURL};
   console.log(urlDatabase[shortURL]);
-  let templateVars = { user_id: req.session.user_id, urls: listLinks(req.session.user_id)};
-  res.render("urls_index",templateVars);       // Respond with 'Ok' (we will replace this)
+  let templateVars = { userId: req.session.userId, urls: listLinks(req.session.userId)};
+  res.render("urls_index", templateVars);
 });
 app.post("/login", (req, res) => {
   
   let loggedin = false;
   for(var x in users){
-    if(users[x]['email'] === req.body['user_id'] && bcrypt.compareSync(req.body['password']),users[x]['password']){
-      req.session.user_id=req.body['user_id'];
-      loggedin=true;
-      }
+    if(users[x]['email'] === req.body['userId'] && bcrypt.compareSync((req.body['password']), users[x]['password'])){
+      req.session.userId = req.body['userId'];
+      loggedin = true;
+    }
   }
   
   if(loggedin){
-    //console.log(req.session.user_id+ " is the cookie value!");
+    //console.log(req.session.userId+ " is the cookie value!");
     //console.log(req.body)
     res.redirect('http://localhost:8080/');
   }else{
@@ -91,52 +113,50 @@ app.post("/login", (req, res) => {
   
 });
 app.post("/logout", (req, res) => {
-  req.session.user_id="";
+  req.session.userId = "";
   console.log("cookies cleared");
   res.redirect('http://localhost:8080/urls');
   
 });
 app.post("/urls/:id", (req, res) => {
-  if(req.session.user_id===undefined ){
-    res.render("partials/_header.ejs",{user_id:undefined});
+  if(req.session.userId === undefined){
+    res.render("partials/_header.ejs", {userId: undefined});
   }
-  console.log("in Post urls/:id");
-  urlDatabase[req.params.id]["longURL"]=req.body['longURL'];
-  let templateVars = { user_id: req.session.user_id, urls: listLinks[req.session.user_id] };
-  res.render("urls_index.ejs",templateVars);
+  urlDatabase[req.params.id]["longURL"] = req.body['longURL'];
+  let templateVars = { userId: req.session.userId, urls: listLinks[req.session.userId] };
+  res.render("urls_index.ejs", templateVars);
 });
 app.post("/urls/:id/delete", (req, res) => {
-  if(req.session.user_id===undefined ){
-    res.render("partials/_header.ejs",{user_id:undefined});
+  if(req.session.userId === undefined ){
+    res.render("partials/_header.ejs", {userId: undefined});
   }
   //console.log(req.params.id);
   console.log("HERE!");
-  let templateVars = { user_id: req.session.user_id, urls: listLinks[req.session.user_id] };
-    delete urlDatabase[req.params.id];
-    console.log(urlDatabase[req.params.id]);
-    res.render('urls_index',templateVars);
+  let templateVars = { userId: req.session.userId, urls: listLinks[req.session.userId] };
+  delete urlDatabase[req.params.id];
+  console.log(urlDatabase[req.params.id]);
+  res.render('urls_index', templateVars);
 });
 app.get("/urls.json", (req, res) => {
-  if(req.session.user_id===undefined ){
-    res.render("partials/_header.ejs",{user_id:undefined});
+  if(req.session.userId === undefined ){
+    res.render("partials/_header.ejs", {userId: undefined});
   }
-  res.json(urlDatabase[req.session.user_id]);
+  res.json(urlDatabase[req.session.userId]);
 });
 app.get("/hello", (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
 });
 app.get("/urls", (req, res) => {
-  if(req.session.user_id===undefined ){
-    res.render("partials/_header.ejs",{user_id:undefined});
+  if(req.session.userId === undefined ){
+    res.render("partials/_header.ejs", {userId: undefined});
   }
-  console.log(req.session.user_id);
-    let templateVars = { user_id: req.session.user_id, urls: listLinks(req.session.user_id) };
-    res.render("urls_index.ejs",templateVars);
-  
+  console.log(req.session.userId);
+  let templateVars = { userId: req.session.userId, urls: listLinks(req.session.userId) };
+  res.render("urls_index.ejs", templateVars);
 });
 app.get("/urls/new", (req, res) => {
   
-  if(req.session.user_id===undefined){
+  if(req.session.userId === undefined){
     res.redirect("http://localhost:8080/login");
   }else{
     res.render("urls_new");
@@ -144,17 +164,16 @@ app.get("/urls/new", (req, res) => {
   
 });
 app.get("/urls/:id", (req, res) => {
-  if(req.session.user_id===undefined ){
-    res.render("partials/_header.ejs",{user_id:undefined});
+  if(req.session.userId === undefined ){
+    res.render("partials/_header.ejs", {userId: undefined});
   }
   let id = req.params.id;
-  let templateVars={};
+  let templateVars = {};
   if(urlDatabase.hasOwnProperty(id)){
-    if(urlDatabase[id]['user_id']===req.session.user_id){
-    
-    templateVars = { user_id: req.session.user_id,response : 'Full URL:' +urlDatabase[id]['longURL']+"  Short form:"+id, shorturl:id,urlfound:true};
+    if(urlDatabase[id]['userId'] === req.session.userId){
+      templateVars = { userId: req.session.userId, response: 'Full URL:' + urlDatabase[id]['longURL'] + "  Short form:" + id, shorturl: id, urlfound: true};
     }else{
-     templateVars = { user_id: req.session.user_id,response :'shorturl not found',urlfound:false}
+      templateVars = { userId: req.session.userId, response: 'shorturl not found', urlfound: false};
     }
   }
   //console.log(templateVars);
@@ -162,21 +181,17 @@ app.get("/urls/:id", (req, res) => {
 });
 app.get("/u/:shortURL", (req, res) => {
   var passed = false;
-  var longURL ="http://localhost:8080";
+  var longURL = "http://localhost:8080";
   var sURL = req.params.shortURL;
- for(var x in urlDatabase){
-   
-   if(urlDatabase.hasOwnProperty(sURL)){
-     console.log(x+" is  "+ sURL);
-     if(x===sURL){
-       console.log("Passed if statement");
-       longURL=urlDatabase[x]['longURL'];
-      
-     }
-   }
- }
- console.log("LONG URL =="+longURL);
- res.redirect(longURL);
+  for(var x in urlDatabase){
+    if(urlDatabase.hasOwnProperty(sURL)){
+      if(x === sURL){
+        longURL = urlDatabase[x]['longURL'];
+      }
+    }
+  }
+  console.log("LONG URL ==" + longURL);
+  res.redirect(longURL);
 
 });
 app.get("/register", (req, res) => {
@@ -185,37 +200,4 @@ app.get("/register", (req, res) => {
 app.get("/login", (req, res) => {
   res.render("login");
 });
-function generateRandomString() {
-  let result = ""
-  for(i=0;i<6;i++){
-    result+=String.fromCharCode(42*Math.random()+48);
-  }
-  console.log(result);
-    return result;
-}
-function generateRandomUsername() {
-  let result = ""
-  for(i=0;i<10;i++){
-    result+=String.fromCharCode(42*Math.random()+48);
-  }
-  console.log(result);
-    return 'usr'+result;
-}
-function listLinks(user_id){
-  let result = [];
-  //console.log("in listLinks");
-  for(var x in urlDatabase){
-    //console.log(x);
-   /* if(x['user_id']===user_id){
-      result.push(x);
-    }*/
-    
-    if(urlDatabase[x]['user_id']===user_id){
-      //console.log(x +"was added");
-      result.push(urlDatabase[x]);
-    }
-    
-  }
-  //console.log(result);
-  return result;
-}
+
