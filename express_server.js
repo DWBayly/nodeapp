3fef3d7e61e5c6b928430d9cfe26ab5c88d7a14e
dbaylyuvic <dbayly@uvic.ca>
2017-11-09 16:40:38 -0800

    After a long slog, I updated short urls to be user specific

--- express_server.js
+++ express_server.js
@@ -8,8 +8,8 @@ var cookieParser = require('cookie-parser');
 app.use(cookieParser());
 
 var urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xk": "http://www.google.com"
+  'b2xVn2':{"longURL": "http://www.lighthouselabs.ca",'user_id':"jack",'shortURL':'b2xVn2'},
+  '9sm5xk':{"longURL": "http://www.google.com",'user_id':"jack",'shortURL':'9sm5xk'}
 };
 
 
@@ -27,18 +27,23 @@ const users={
 }
 //GET METHODS
 app.get("/", (req, res) => {
-  res.end("Hello!");
+  if(req.cookies['user_id']===undefined){
+    res.render("partials/_header.ejs",{user_id:undefined});
+  }else{
+    res.render("partials/_header.ejs",{user_id:req.cookies['user_id']});
+  }
 });
 
+ 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 app.post("/register", (req, res) => {
   console.log(req.body);
-  res.cookie('user_id',req.body['user_id'],{});
   let nme = req.body['user_id'];
   let pwd = req.body['password'];
   let id = generateRandomUsername();
+  res.cookie('user_id',id,{});
   if(nme!=="" && pwd!==""){
     users[id] = {id:id,email:nme,password:pwd}
     res.redirect('http://localhost:8080/urls');
@@ -47,17 +52,22 @@ app.post("/register", (req, res) => {
   }
 });
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // debug statement to see POST parameters
+   if(req.cookies['user_id']===undefined ){
+    res.render("partials/_header.ejs",{user_id:undefined});
+  }
+  //console.log(req.body);  // debug statement to see POST parameters
   let shortURL = generateRandomString();
-  urlDatabase[shortURL] = req.body['longURL'];
-  res.send(shortURL);         // Respond with 'Ok' (we will replace this)
+  urlDatabase[shortURL]={'longURL':req.body['longURL'],'user_id':req.cookies['user_id'],'shortURL':shortURL};
+  console.log(urlDatabase[shortURL]);
+  let templateVars = { user_id: req.cookies["user_id"], urls: listLinks(req.cookies['user_id'])};
+  res.render("urls_index",templateVars);       // Respond with 'Ok' (we will replace this)
 });
 app.post("/login", (req, res) => {
   
   let loggedin = false;
   for(var x in users){
     if(users[x]['email'] === req.body['user_id'] && users[x]['password']===req.body['password']){
-      res.cookie('user_id',req.body['user_id'],{});
+      res.cookie('user_id',users[x][id],{});
       loggedin=true;
       }
   }
@@ -79,54 +89,87 @@ app.post("/logout", (req, res) => {
   
 });
 app.post("/urls/:id", (req, res) => {
-  console.log(req.body);
-  urlDatabase[req.params.id]=req.body['longURL'];
-    let templateVars = { user_id: req.cookies["user_id"], urls: urlDatabase };
-    res.redirect('http://localhost:8080/urls')
+  if(req.cookies['user_id']===undefined ){
+    res.render("partials/_header.ejs",{user_id:undefined});
+  }
+  console.log("in Post urls/:id");
+  urlDatabase[req.params.id]["longURL"]=req.body['longURL'];
+  let templateVars = { user_id: req.cookies["user_id"], urls: listLinks[req.cookies['user_id']] };
   res.render("urls_index.ejs",templateVars);
 });
 app.post("/urls/:id/delete", (req, res) => {
-  console.log(req.params.id);
-  console.log("HERE!");
-  if(urlDatabase.hasOwnProperty(req.params.id)){
-    delete urlDatabase[req.params.id];
-    console.log(urlDatabase);
-    res.redirect('http://localhost:8080/urls/new');
+  if(req.cookies['user_id']===undefined ){
+    res.render("partials/_header.ejs",{user_id:undefined});
   }
+  //console.log(req.params.id);
+  console.log("HERE!");
+  let templateVars = { user_id: req.cookies["user_id"], urls: listLinks[req.cookies['user_id']] };
+    delete urlDatabase[req.params.id];
+    console.log(urlDatabase[req.params.id]);
+    res.render('urls_index',templateVars);
 });
 app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+  if(req.cookies['user_id']===undefined ){
+    res.render("partials/_header.ejs",{user_id:undefined});
+  }
+  res.json(urlDatabase[req.cookies['user_id']]);
 });
 app.get("/hello", (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
 });
 app.get("/urls", (req, res) => {
-  let templateVars = { user_id: req.cookies["user_id"], urls: urlDatabase };
-  res.render("urls_index.ejs",templateVars);
+  if(req.cookies['user_id']===undefined ){
+    res.render("partials/_header.ejs",{user_id:undefined});
+  }
+  console.log(req.cookies['user_id']);
+    let templateVars = { user_id: req.cookies["user_id"], urls: listLinks(req.cookies["user_id"]) };
+    res.render("urls_index.ejs",templateVars);
+  
 });
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  
+  if(req.cookies["user_id"]===undefined){
+    res.redirect("http://localhost:8080/login");
+  }else{
+    res.render("urls_new");
+  }
+  
 });
 app.get("/urls/:id", (req, res) => {
-  //let templateVars = { shortURL: req.params.id,urls:urlDatabase};
+  if(req.cookies['user_id']===undefined ){
+    res.render("partials/_header.ejs",{user_id:undefined});
+  }
   let id = req.params.id;
   let templateVars={};
-  if(urlDatabase.hasOwnProperty(id)){
-    templateVars = { user_id: req.cookies["user_id"],response : 'Full URL:' +urlDatabase[id]+"  Short form:"+id, shorturl:id,urlfound:true};
-  }else{
-   templateVars = { user_id: req.cookies["user_id"],response :'shorturl not found',urlfound:false}
+  if(urlDatabase.hasOwnProperty(id){
+    if(urlDatabase[id]['user_id']===req.cookies['user_id']){
+    
+    templateVars = { user_id: req.cookies["user_id"],response : 'Full URL:' +urlDatabase[id]['longURL']+"  Short form:"+id, shorturl:id,urlfound:true};
+    }else{
+     templateVars = { user_id: req.cookies["user_id"],response :'shorturl not found',urlfound:false}
+    }
   }
   //console.log(templateVars);
   res.render("urls_show", templateVars);
 });
 app.get("/u/:shortURL", (req, res) => {
-  let longURL  = 'localhost:8080/urls';
-  if(urlDatabase.hasOwnProperty(req.params.shortURL)){
-      //console.log("FOUND!");
-      longURL= urlDatabase[req.params.shortURL];
-  }
-  //console.log(longURL);
-  res.redirect(longURL);
+  var passed = false;
+  var longURL ="http://localhost:8080";
+  var sURL = req.params.shortURL;
+ for(var x in urlDatabase){
+   
+   if(urlDatabase.hasOwnProperty(sURL)){
+     console.log(x+" is  "+ sURL);
+     if(x===sURL){
+       console.log("Passed if statement");
+       longURL=urlDatabase[x]['longURL'];
+      
+     }
+   }
+ }
+ console.log("LONG URL =="+longURL);
+ res.redirect(longURL);
+
 });
 app.get("/register", (req, res) => {
   res.render("register");
@@ -150,4 +193,21 @@ function generateRandomUsername() {
   console.log(result);
     return 'usr'+result;
 }
-
+function listLinks(user_id){
+  let result = [];
+  //console.log("in listLinks");
+  for(var x in urlDatabase){
+    //console.log(x);
+   /* if(x['user_id']===user_id){
+      result.push(x);
+    }*/
+    
+    if(urlDatabase[x]['user_id']===user_id){
+      //console.log(x +"was added");
+      result.push(urlDatabase[x]);
+    }
+    
+  }
+  //console.log(result);
+  return result;
+}
